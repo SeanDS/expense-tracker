4971df21eaf5743c90de1f2b4f2f48ac68f9a056
Sean Leavey <sean.leavey@gmail.com>
2015-10-03 16:45:00 +0100

    Move totals to top of index page.

--- templates/expenses.php
+++ templates/expenses.php
@@ -1,5 +1,5 @@
 <?php $this->layout('expenses-template') ?>
+<?php $this->insert('expenses-totals', ['totals' => $totals]) ?>
 <p><a href="index.php?do=new" class="btn btn-success">New</a></p>
 <?php $this->insert('expenses-list', ['expenses' => $expenses, 'message' => $message]) ?>
-<?php $this->insert('expenses-totals', ['totals' => $totals]) ?>
 <p><a href="index.php?do=new" class="btn btn-success">New</a></p>
